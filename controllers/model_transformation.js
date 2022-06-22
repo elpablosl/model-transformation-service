0eb220ff60fe951ec3823648f8873e5e03b06fdb
Pablo Soto <pablo.sotolib@gmail.com>
2022-06-22 15:01:52 -0400

    extract class for CA JSON Model

--- controllers/model_transformation.js
+++ controllers/model_transformation.js
@@ -1,7 +1,17 @@
 const { response, request } = require('express');
 
-const modelTransformationPost = (req, res = response) => {
+const modelTransformationPost = (req = request, res = response) => {
  
+   const { communicativeInteractions } = req.body;
+
+   let UML_CLASS = [];
+
+   for( let i = 0; i< communicativeInteractions.length; i++ ) {
+    
+         UML_CLASS.push({ class_name: communicativeInteractions[i].name })
+
+   }
+
      res.json({
          msg: 'post API - Controlador',
      });
