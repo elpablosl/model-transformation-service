3ab0ac088586339dfdbd43c7e5eaaec603c8bedb
Pablo Soto <pablo.sotolib@gmail.com>
2022-06-24 17:09:18 -0400

    start class relations

--- controllers/model_transformation.js
+++ controllers/model_transformation.js
@@ -7,13 +7,24 @@ const modelTransformationPost = (req = request, res = response) => {
    let UML_CLASS = [];
 
    communicativeInteractions.forEach(element => {
-      UML_CLASS.push({ class_name: element.name, attributes: [] })
       element.messageStructure.children.forEach(item => {
-         const indice = UML_CLASS.findIndex(item => item.class_name === element.name );
-         UML_CLASS[indice].attributes.push({ name: item.name, domain: item.domain })
+         UML_CLASS.push({ class_name: item.name, attributes: [] });
+         item.children.forEach( item_children => {
+            if( item_children.type === 'Data Field') {
+               const indice = UML_CLASS.findIndex(el => el.class_name === item.name);
+               UML_CLASS[indice].attributes.push({ name: item_children.name, domain: item_children.domain });
+            } else if(item_children.type === 'Reference Field') {
+               console.log(item_children);
+            }
+         })
+         // const indice = UML_CLASS.findIndex(item => item.class_name === element.name );
+         // UML_CLASS[indice].attributes.push({ name: item.name, domain: item.domain })
       });
    });
 
+
+   let myJSON = JSON.stringify(UML_CLASS);
+   console.log(myJSON);
 
      res.json({
          msg: 'post API - Controlador',
