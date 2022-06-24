aae7c16d63f08e59cda54cdef73d7bec303f2698
Pablo Soto <pablo.sotolib@gmail.com>
2022-06-24 12:02:26 -0400

    extract attributes from classes

--- controllers/model_transformation.js
+++ controllers/model_transformation.js
@@ -6,11 +6,14 @@ const modelTransformationPost = (req = request, res = response) => {
 
    let UML_CLASS = [];
 
-   for( let i = 0; i< communicativeInteractions.length; i++ ) {
-    
-         UML_CLASS.push({ class_name: communicativeInteractions[i].name })
+   communicativeInteractions.forEach(element => {
+      UML_CLASS.push({ class_name: element.name, attributes: [] })
+      element.messageStructure.children.forEach(item => {
+         const indice = UML_CLASS.findIndex(item => item.class_name === element.name );
+         UML_CLASS[indice].attributes.push({ name: item.name, domain: item.domain })
+      });
+   });
 
-   }
 
      res.json({
          msg: 'post API - Controlador',
