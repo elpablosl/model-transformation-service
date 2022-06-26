c15ff5a00fa2fb4a9f4d19c9f8b6eec1d0ed7504
Pablo Soto <pablo.sotolib@gmail.com>
2022-06-26 14:19:44 -0400

    added class relations with cardinalities

--- controllers/model_transformation.js
+++ controllers/model_transformation.js
@@ -8,23 +8,27 @@ const modelTransformationPost = (req = request, res = response) => {
 
    communicativeInteractions.forEach(element => {
       element.messageStructure.children.forEach(item => {
-         UML_CLASS.push({ class_name: item.name, attributes: [] });
+         UML_CLASS.push({ class_name: item.name, attributes: [], class_relations: [] });
          item.children.forEach( item_children => {
             if( item_children.type === 'Data Field') {
-               const indice = UML_CLASS.findIndex(el => el.class_name === item.name);
+               let indice = UML_CLASS.findIndex(el => el.class_name === item.name);
                UML_CLASS[indice].attributes.push({ name: item_children.name, domain: item_children.domain });
             } else if(item_children.type === 'Reference Field') {
-               console.log(item_children);
+               let indice = UML_CLASS.findIndex(el => el.class_name === item.name);
+               UML_CLASS[indice].class_relations.push({ class_source: item.name, class_target: item_children.name, class_cardinality: '[0..1]' });
+
+               indice = UML_CLASS.findIndex(el => el.class_name === item_children.name);
+               UML_CLASS[indice].class_relations.push({ class_source: item_children.name, class_target: item.name, class_cardinality: '[0..*]' })
+
             }
          })
-         // const indice = UML_CLASS.findIndex(item => item.class_name === element.name );
-         // UML_CLASS[indice].attributes.push({ name: item.name, domain: item.domain })
       });
    });
 
+      let myJSON = JSON.stringify(UML_CLASS);
+      console.log(myJSON);
 
-   let myJSON = JSON.stringify(UML_CLASS);
-   console.log(myJSON);
+   
 
      res.json({
          msg: 'post API - Controlador',
