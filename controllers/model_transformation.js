5481e22ac8fee9202ad045a2001319269c7cc3e6
Pablo Soto <pablo.sotolib@gmail.com>
2022-07-04 16:48:43 -0400

    added nested aggregation and fixed error on reference field if not extend ebj

--- controllers/model_transformation.js
+++ controllers/model_transformation.js
@@ -11,14 +11,13 @@ const modelTransformationPost = (req = request, res = response) => {
       communicativeInteractions.forEach(element => {
 
          element.messageStructure.children.forEach(item => {
-            
-            UML_CLASS.push({ class_name: item.name, attributes: [], class_relations: [] });
+                     
+               UML_CLASS.push({ class_name: item.name, attributes: [], class_relations: [] });
 
-            let indice = UML_CLASS.findIndex(el => el.class_name === item.name);
-            UML_CLASS[indice].attributes.push({ attribute_name: item.name + '_ID', attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
-            
+               let indice = UML_CLASS.findIndex(el => el.class_name === item.name);
+               UML_CLASS[indice].attributes.push({ attribute_name: item.name + '_ID', attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+      
             item.children.forEach( item_children => {
-   
 
                if( item_children.type === 'Data Field') {
 
@@ -39,22 +38,20 @@ const modelTransformationPost = (req = request, res = response) => {
                      if( item_children.domain === 'Money' ) {
                         UML_CLASS[indice].attributes.push({ attribute_name: item_children.name, attribute_domain: 'Date', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No'})
                      }
-
-                     // UML_CLASS[indice].attributes.push({ name: item_children.name, domain: item_children.domain });
                
                   } 
                   
                   if(item_children.type === 'Reference Field' && item_children.extends_business_object === 'False' ) {
 
                      let indice = UML_CLASS.findIndex(el => el.class_name === item.name);
-                     UML_CLASS[indice].class_relations.push({ class_source: item.name, class_target: item_children.name, class_cardinality: '[0..1]' });
+                     UML_CLASS[indice].class_relations.push({ class_source: item.name, class_target: item_children.name, class_cardinality: '[0..*]' });
    
           
                      indice = UML_CLASS.findIndex(el => el.class_name === item_children.name);
                      if( indice === -1 ) {
                         throw 'Break';
                      }
-                     UML_CLASS[indice].class_relations.push({ class_source: item_children.name, class_target: item.name, class_cardinality: '[0..*]' });
+                     UML_CLASS[indice].class_relations.push({ class_source: item_children.name, class_target: item.name, class_cardinality: '[0..1]' });
                      
                } else if( item_children.type === 'Reference Field' && item_children.extends_business_object === 'True' ) {
 
@@ -88,8 +85,7 @@ const modelTransformationPost = (req = request, res = response) => {
                }
    
                if(item_children.type === 'Iteration') {
-   
-   
+
                   item_children.children.forEach( iteration_children => {
    
                         UML_CLASS.push({ class_name: iteration_children.name, attributes: [], class_relations: [] });
@@ -123,10 +119,46 @@ const modelTransformationPost = (req = request, res = response) => {
                            UML_CLASS[indice].attributes.push({ attribute_name: children_iteration.name, attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
                         }
    
-                     })
+                     });
    
-                  })
-   
+                  });
+               }
+
+               if( item_children.type === 'Aggregation' ) {
+
+                  UML_CLASS.push({ class_name: item_children.name, attributes: [], class_relations: [] });
+
+                  let indice_aggregation = UML_CLASS.findIndex(el => el.class_name === item_children.name);
+                  UML_CLASS[indice_aggregation].attributes.push({ attribute_name: item_children.name + '_ID', attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+
+                  indice_aggregation = UML_CLASS.findIndex(el => el.class_name === item_children.name );
+                  UML_CLASS[indice_aggregation].class_relations.push({ class_source: item_children.name, class_target: item.name, class_cardinality: '[0..1]' });
+
+                  indice_aggregation = UML_CLASS.findIndex(el => el.class_name === item.name );
+                  UML_CLASS[indice_aggregation].class_relations.push({ class_soruce: item.name, class_target: item_children.name, class_cardinality: '[0..1]' });
+
+                  item_children.children.forEach( aggregation_children => {
+
+                     let indice_agregar = UML_CLASS.findIndex(el => el.class_name === item_children.name );
+
+                     if( aggregation_children.domain === 'Number' ) {
+                        UML_CLASS[indice_agregar].attributes.push({ attribute_name: aggregation_children.name, attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+                     }    
+                     
+                     if( aggregation_children.domain === 'Text' ) {
+                        UML_CLASS[indice_agregar].attributes.push({ attribute_name: aggregation_children.name, attribute_domain: 'String', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+                     }
+
+                     if( aggregation_children.domain === 'Date' ) {
+                        UML_CLASS[indice_agregar].attributes.push({ attribute_name: aggregation_children.name, attribute_domain: 'Date', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+                     }   
+                     
+                     if( aggregation_children.domain === 'Money' ) {
+                        UML_CLASS[indice_agregar].attributes.push({ attribute_name: aggregation_children.name, attribute_domain: 'Integer', attribute_type: 'const', requested_creation: 'Yes', null_allowed: 'No' });
+                     } 
+
+                  });
+
                }
    
             });
