73aa10d29ad5e0a4d8e88220ddf076f77df36e99
Pablo Soto <pablo.sotolib@gmail.com>
2022-09-13 15:48:53 -0300

    added check middleware to the route

--- routes/model_transformation.js
+++ routes/model_transformation.js
@@ -1,10 +1,23 @@
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { modelTransformationPost } = require('../controllers/model_transformation');
 
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const router = Router();
 
-router.post('/', modelTransformationPost );
+router.post('/', [
+    check('header', 'Se debe ingresar el header').not().isEmpty(),
+    check('actors', 'Se deben ingresar los actores').not().isEmpty(),
+    check('starts', 'Se debe ingresar el nodo de inicio').not().isEmpty(),
+    check('ends', 'Se debe ingresar el nodo final').not().isEmpty(),
+    check('communicativeEvents', 'Se debe ingresar la información del evento comunicativo').not().isEmpty(),
+    check('communicativeEvents.*.unique','Se debe ingresar la ID del evento comunicativo').not().isEmpty(),
+    check('precedenceRelations','Se debe ingresar las relaciones de precedencia').not().isEmpty(),
+    check('communicativeInteractions','Se debe ingresar las interacciones comunicativas').not().isEmpty(),
+    validarCampos
+], modelTransformationPost );
 
 module.exports = router;
