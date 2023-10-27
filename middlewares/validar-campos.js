a7b0339f1d02f7bd16ca9cc5658358612d706ba5
elpablosl <pablo.sotolib@gmail.com>
2023-10-27 12:36:37 -0300

    cambios en validarCampos

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -1,16 +1,27 @@
 const { response, request } = require('express');
 const { validationResult } = require('express-validator');
 
-const validarCampos = ( req = request, res = response, next ) => {
+const customErrorFormatter = ({ location, msg, param }) => {
+    // Customize the error format here as you want
+    return {
+        msg,
+        param,
+        location,
+    };
+};
 
+const validarCampos = (req = request, res = response, next) => {
     const errors = validationResult(req);
-    if( !errors.isEmpty() ) {
-        return res.status(400).json(errors);
+
+    if (!errors.isEmpty()) {
+        // Format the errors using the custom error formatter
+        const formattedErrors = errors.formatWith(customErrorFormatter).array();
+
+        return res.status(400).json({ errors: formattedErrors });
     }
 
     next();
-
-}
+};
 
 module.exports = {
     validarCampos
